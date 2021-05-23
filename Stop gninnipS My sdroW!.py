c1c3f95da92ab4c78e250c60bf055879a3a03aee
GedikEngin <74453024+GedikEngin@users.noreply.github.com>
2021-05-23 11:04:53 +0100

    chore: implement better practice and smarter solutions at the end

--- Stop gninnipS My sdroW!.py
+++ Stop gninnipS My sdroW!.py
@@ -30,3 +30,18 @@ if __name__ == '__main__':
 # https://www.geeksforgeeks.org/python-reverse-word-sentence/
 # https://www.statology.org/replace-values-in-list-python/
 # https://www.askpython.com/python/string/remove-character-from-string-python
+
+# solution 1:
+'''
+def spin_words(sentence):
+    words = [word for word in sentence.split(" ")]
+    words = [word if len(word) < 5 else word[::-1] for word in words]
+    return " ".join(words)
+'''
+
+# solution 2:
+'''
+def spin_words(sentence):
+    # Your code goes here
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+'''
