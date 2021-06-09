c9773a0e97a9ffec0bd06e7a7a555c16436ed2d3
GedikEngin <74453024+GedikEngin@users.noreply.github.com>
2021-06-09 10:04:36 +0100

    feature: create an sorted string using unique characters from 2 input strings

---
+++ Two To One.py
@@ -0,0 +1,22 @@
+def longest(a1, a2):
+    string = ''
+    for char in a1:
+        if char not in string:
+            string += char
+    for char in a2:
+        if char not in string:
+            string += char
+    ''.join(sorted(string))
+    return print(string)
+
+if __name__ == '__main__':
+    longest("aretheyhere", "yestheyarehere")
+    longest("loopingisfunbutdangerous", "lessdangerousthancoding")
+
+# story:
+# for i in a1/a2
+# check if it is in the final string
+# add to the string if not in it
+
+# sources:
+# https://www.geeksforgeeks.org/python-ways-to-sort-letters-of-string-alphabetically/
